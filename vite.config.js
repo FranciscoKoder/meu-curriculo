a65c51ecea8484f5ed7034cf8c48142178d431b3
dvlpchico-s <vitorfariasdev@gmail.com>
2025-05-26 23:19:20 -0300

    feat: adicionar imagem de pré-visualização e configurar base no Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/meu-curriculo/', // coloque o nome do seu repositório aqui, com as barras
   plugins: [react()],
 })
